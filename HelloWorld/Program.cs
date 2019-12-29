6c902843854f469be6b1a86edad8149e32740958
athelasar@gmail.com <athelasar@gmail.com>
2019-12-29 12:02:59 +0100

    Cleaning up

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Hello_World
+namespace HelloWorld
 {
     public class Program
     {
