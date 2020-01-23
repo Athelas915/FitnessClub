7644bfc59d96a96af85b9c60e887db070df39acc
Wojciech Grabowski <athelasar@gmail.com>
2020-01-24 00:41:51 +0100

    Adding page for google authorization

---
+++ HelloWorld/Pages/google-login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorld.Pages
+{
+    public class GoogleSigninModel : PageModel
+    {
+        private readonly ILogger<GoogleSigninModel> _logger;
+
+        public GoogleSigninModel(ILogger<GoogleSigninModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
