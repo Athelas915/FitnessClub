d324480ffe4b37e25ca5a155c16d83940b824338
Wojciech Grabowski <athelasar@gmail.com>
2020-01-24 01:45:45 +0100

    Cleaning up and making a sign-up with google page

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace HelloWorld
+namespace FitnessClub
 {
     public class Program
     {
