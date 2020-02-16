7b042d1a2abcf3f58b03f3146b35ae7966610330
Wojciech Grabowski <athelasar@gmail.com>
2020-02-16 16:15:35 +0100

    Adding generic repository to not repeat code for other entities' repositories.

---
+++ FitnessClub/Data/DAL/GenericRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using FitnessClub.Data.Models;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Data.DAL
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal FCContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(FCContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual async Task<IList<TEntity>> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+        public virtual async Task<TEntity> GetByID(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+        public virtual void Delete(int id)
+        {
+            TEntity entity = dbSet.Find(id);
+            dbSet.Remove(entity);
+        }
+        public virtual void Delete(TEntity entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+        public virtual void UpdatePerson(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+            return;
+        }
+
+        public bool Any(int id)
+        {
+            TEntity entity = dbSet.Find(id);
+            if (entity == null) { return false; }
+            else { return true; }
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ FitnessClub/Data/DAL/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using FitnessClub.Data.Models;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Data.DAL
+{
+    public interface IGenericRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task<IList<TEntity>> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        Task<TEntity> GetByID(int id);
+        void Insert(TEntity entity);
+        void Delete(int id);
+        void Delete(TEntity entity);
+        void UpdatePerson(TEntity entity);
+        Task Save();
+        bool Any(int id);
+
+    }
+}
