5067c05fb55b63a7d169ebb4b86d2a349b246d1b
Wojciech Grabowski <athelasar@gmail.com>
2020-02-26 15:25:19 +0100

    Fixing same bug

--- FitnessClub/Data/Models/BaseEntity.cs
+++ FitnessClub/Data/Models/BaseEntity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace FitnessClub.Data.Models
 {
     public class BaseEntity
     {
+        [ReadOnly(true)]
         public DateTime CreatedOn { get; set; }
         public int CreatedBy { get; set; }
     }
