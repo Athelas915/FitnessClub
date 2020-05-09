aebc7bdbcb164a0b6f32bd9cfbc2184935bece63
Wojciech Grabowski <athelasar@gmail.com>
2020-05-09 23:00:29 +0200

    Testing new CI setup that automatically triggers on pull requests

--- FitnessClub/Startup.cs
+++ FitnessClub/Startup.cs
@@ -52,7 +52,6 @@ namespace FitnessClub
                 Configuration["DefaultAdminPassword"] = Environment.GetEnvironmentVariable("DefaultPassword");
             }
         }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
